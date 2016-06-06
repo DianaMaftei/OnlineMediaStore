1c587d70da5793a969a9daeb0249fe962f43d98b
Diana <Diana@Mine>
2016-06-06 21:53:16 +0300

    start of new project

---
+++ Online Media Store/src/DigitalVideoDisc.java
@@ -0,0 +1,14 @@
+
+public class DigitalVideoDisc {
+	
+	private String title; 
+	
+	private String category; 
+	
+	private double cost;
+	
+	private String director; 
+
+	private double length;
+
+}
